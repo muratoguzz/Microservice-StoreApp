0b316f5726d76141543d0b206b764dea3bcaeef2
Murat Oğuz <muratouz95@gmail.com>
2024-11-08 14:04:15 +0300

    Fixed bug in productdetails controller

--- Services/Catalog/StoreApp.Catalog/Program.cs
+++ Services/Catalog/StoreApp.Catalog/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.Extensions.Options;
+using StoreApp.Catalog.Services.CategoryServices;
+using StoreApp.Catalog.Services.ProductDetailServices;
+using StoreApp.Catalog.Services.ProductImageServices;
+using StoreApp.Catalog.Services.ProductServices;
+using StoreApp.Catalog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value; //appsettingjson dosyasýndaki databasesettings keyinin valueleri alýndý
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Services/Catalog/StoreApp.Catalog/obj/Debug/net6.0/StoreApp.Catalog.AssemblyInfo.cs
+++ Services/Catalog/StoreApp.Catalog/obj/Debug/net6.0/StoreApp.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a58317b1bb67d186ed76fcdbb44c88bbf5e22d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
