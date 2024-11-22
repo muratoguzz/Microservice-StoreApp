81d521b0867222049d491f7a647668cdae746e1f
Murat Oğuz <muratouz95@gmail.com>
2024-11-23 02:51:48 +0300

    home page completed

---
+++ Frontends/StoreApp.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial : ViewComponent
+    {
+        //private readonly IAboutService _aboutService;
+        //public _FooterUILayoutComponentPartial(IAboutService aboutService)
+        //{
+        //    _aboutService = aboutService;
+        //}
+        //public async Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    var values = await _aboutService.GetAllAboutAsync();
+        //    return View(values);
+        //}
+    }
+}
